8e4a3f10de74460ba78193703616819129ecca64
Joey <joeyramirez1000@gmail.com>
2022-12-29 12:12:55 +0800

    -Added Appcenter dependencies -Initialize Appcenter crash and analyitics

--- app/src/main/java/com/example/cicdcompose/MainActivity.kt
+++ app/src/main/java/com/example/cicdcompose/MainActivity.kt
@@ -3,18 +3,36 @@ package com.example.cicdcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.cicdcompose.ui.theme.CICDComposeTheme
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        installSplashScreen()
+
+        AppCenter.start(
+            application, "136e0d57-eb44-4f55-8bca-cb01bee0482d",
+            Analytics::class.java, Crashes::class.java
+        )
+
         setContent {
             CICDComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -31,7 +49,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    val items = listOf<String>("1","2","3")
+    
+    LazyColumn{
+        items(items.size){index ->
+            Column (modifier = Modifier.padding(bottom= 8.dp)){
+                Button(onClick = { throw  Exception("hello!") }) {
+                    Text(text = "Hello ${items[index]}")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
